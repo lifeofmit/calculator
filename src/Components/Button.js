60266fd10e400abdffe4162eb98a77df5cd17add
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-08 15:42:31 +0530

    + - x /  this four calculation are working now.

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -27,14 +27,67 @@ const Button = ({ value}) => {
     const resetClick = () => {
         setCalc({ sign: '', num: 0, res: 0 })
     }
+    // 0 to 9 numbers
+    const handleClickButton = () => {
+        const numberString = value.toString()
+
+        let numberValue;
+        if(numberString === '0' && calc.num === 0) {
+            numberValue = "0"
+        } else {
+            numberValue = Number(calc.num + numberString)
+        }
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+    // User Click Operation
+    const signClick = () => {
+        setCalc({
+            sign: value,
+            res: !calc.res && calc.num ? calc.num : calc.res,
+            num: 0
+        })
+    }
+    // Equal Click Operation
+    const equalsClick = () => {
+        if(calc.res && calc.num) {
+            const math = (a, b, sign) => {
+                const result = {
+                    '+': (a, b) => a + b,
+                    '-': (a, b) => a - b,
+                    'x': (a, b) => a * b,
+                    '/': (a, b) => a / b,
+                }
+                return result[sign](a, b);
+            }
+            setCalc({
+                res: math(calc.res, calc.num, calc.sign),
+                sign: '',
+                num: 0
+            })
+        }
+        
+    }
 
     const handleBtnClick = () => {
         
         const results = {
             '.': commaClick,
             'C': resetClick,
+            '/': signClick,
+            'x': signClick,
+            '-': signClick,
+            '+': signClick,
+            '=': equalsClick
         }
-        return results[value]()
+        if(results[value]) {
+            return results[value]()
+        } else {
+            return handleClickButton()
+        }
     }
 
   return (
