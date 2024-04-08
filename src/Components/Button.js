a9db7754dd7fc5600a82603c4ef2fed6da1ac8ee
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-08 13:51:19 +0530

    Initial setup and all buttons values added.

---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+
+
+const Button = ({ value}) => {
+  return (
+    <button>{value}</button>
+  )
+}
+
+export default Button
---
+++ src/Components/Screen.js
@@ -0,0 +1,9 @@
+
+
+const Screen = () => {
+  return (
+    <div className="screen">1654658</div>
+  )
+}
+
+export default Screen
